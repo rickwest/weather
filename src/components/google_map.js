fd606fab3050a32a81caff6d45c05781d7d0a259
Richard West <rw@tonic.works>
2017-03-30 11:29:27 +0100

    Fixed google map issue

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
 class GoogleMap extends Component {
-    componentDidMount(){
+    componentDidMount() {
         new google.maps.Map(this.refs.map, {
             zoom: 12,
             center: {
                 lat: this.props.lat,
                 lng: this.props.lon
             }
-        })
+        });
     }
 
     render() {
